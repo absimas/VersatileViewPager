24620b9ce462b948f017d5fb0fc06d6b864ed4dc
absimas <ab.simas@gmail.com>
2015-06-26 22:11:36 +0300

    Rename lib to versatile_view_pager.

--- app/src/androidTest/java/com/simas/versatileviewpager/sample/TestUtils.java
+++ app/src/androidTest/java/com/simas/versatileviewpager/sample/TestUtils.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2015. Simas Abramovas
+ *
+ * This file is part of VersatileViewPager.
+ *
+ * VersatileViewPager is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * VersatileViewPager is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with VersatileViewPager. If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.simas.versatileviewpager.sample;
 
 import android.support.test.InstrumentationRegistry;
@@ -6,26 +24,6 @@ import static android.support.test.espresso.Espresso.*;
 import static android.support.test.espresso.assertion.ViewAssertions.*;
 import static android.support.test.espresso.matcher.ViewMatchers.*;
 
-
-/**
- * VersatileViewPager
- * Copyright (c) 2015 Simas Abramovas
- * <p/>
- * This file is part of VersatileViewPager.
- * <p/>
- * VersatileViewPager is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * <p/>
- * VersatileViewPager is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * <p/>
- * You should have received a copy of the GNU General Public License
- * along with VersatileViewPager.  If not, see <http://www.gnu.org/licenses/>.
- */
 public class TestUtils {
 
 	public static final ViewAssertion IS_DISPLAYED_ASSERTION = matches(isDisplayed());
